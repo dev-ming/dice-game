1db962ee8cc91400d979b1c7da42950f28ab2634
Jenna <ruaaaa@ajou.ac.kr>
2022-07-14 17:17:27 +0900

    :lipstick: update style

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Board from "./Board";
 import Button from "./Button";
 import "./App.css";
+import logo from "./assets/logo.png";
 
 function random(n) {
   return Math.ceil(Math.random() * n);
@@ -17,6 +18,8 @@ function App() {
 
     setMyHistory([...myHistory, nextMyNum]);
     setOtherHistory([...otherHistory, nextOtherNum]);
+
+    setWinner();
   };
 
   const handleClearClick = () => {
@@ -24,8 +27,34 @@ function App() {
     setOtherHistory([]);
   };
 
+  let myClassName = "Board";
+  let otherClassName = "Board";
+
+  const setWinner = () => {
+    let myClassNameTemp = "Board";
+    let otherClassNameTemp = "Board";
+    if (
+      myHistory[myHistory.length - 1] > otherHistory[otherHistory.length - 1]
+    ) {
+      myClassNameTemp += " Board-winner";
+    } else if (
+      myHistory[myHistory.length - 1] < otherHistory[otherHistory.length - 1]
+    ) {
+      otherClassNameTemp += " Board-winner";
+    }
+    myClassName = myClassNameTemp;
+    otherClassName = otherClassNameTemp;
+
+    console.log(myClassName);
+    console.log(otherClassName);
+  };
+
   return (
     <div className="App">
+      <div>
+        <img class="App-logo" src={logo} alt="주사위게임 로고" />
+        <h1 class="App-title">Dice Game</h1>
+      </div>
       <div>
         <Button className="App-button" color="blue" onClick={handleRollClick}>
           던지기
@@ -34,9 +63,21 @@ function App() {
           처음부터
         </Button>
       </div>
-      <div>
-        <Board name="나" color="blue" gameHistory={myHistory} />
-        <Board name="상대" color="red" gameHistory={otherHistory} />
+      <div className="App-boards">
+        <Board
+          className="App-board"
+          a={myClassName}
+          name="나"
+          color="blue"
+          gameHistory={myHistory}
+        />
+        <Board
+          className="App-board"
+          a={otherClassName}
+          name="상대"
+          color="red"
+          gameHistory={otherHistory}
+        />
       </div>
     </div>
   );
